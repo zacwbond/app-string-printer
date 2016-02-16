5d6518e88d50b41937d5a1f6c13ea5e3a36d3601
Zachary Bond <zachary.bond@bigasssolutions.com>
2016-02-16 14:36:05 -0500

    Update to new version of lib; use its functions

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-# Because I am an idiot and used dashes in the name...
 import sys
 from lib import append
 
@@ -31,6 +29,6 @@ ascii_iphone = '''
 def align_text(s):
   return s + ' ' * (13 - len(s)) + '|'
   
+text = append.append_efg(sys.argv[1])
 
-
-print(ascii_iphone.format(align_text(append.append_bcd(sys.argv[1]))))
+print(ascii_iphone.format(align_text(text)))
