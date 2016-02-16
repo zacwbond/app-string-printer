384d428eac4597881486795de2fe3b78763a69b7
Zachary Bond <zachary.bond@bigasssolutions.com>
2016-02-16 14:42:26 -0500

    new lib version is odorous

--- app.py
+++ app.py
@@ -29,6 +29,6 @@ ascii_iphone = '''
 def align_text(s):
   return s + ' ' * (13 - len(s)) + '|'
   
-text = append.append_efg(sys.argv[1])
+text = append.append_stinker(sys.argv[1])
 
 print(ascii_iphone.format(align_text(text)))
